462211bdf71e8867444737fcbebed7ec9528de4b
Yohanes Gultom <yohanes.gultom@gmail.com>
2021-01-16 13:38:23 +0700

    first commit

--- README.md
+++ README.md
@@ -1,21 +1,33 @@
-![Test](https://github.com/ravsamhq/notify-slack-action/workflows/Test/badge.svg)
+![Test](https://github.com/yohanesgultom/notify-slack-action/workflows/Test/badge.svg)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 # Notify Slack Action
 
-Send Github Actions workflow status notifications to Slack regarding failures, warnings or even success. You can read more about the action in [our blog post](https://www.ravsam.in/blog/send-slack-notification-when-github-actions-fails/).
+Send Github Actions workflow status notifications to Slack.
 
 ### Example workflow
 
+Put this step at the end of your workflow:
+
 ```yaml
 steps:
-  - uses: ravsamhq/notify-slack-action@master
+
+  # your other steps
+  # your other steps
+  # your other steps
+
+  - uses: yohanesgultom/notify-slack-action@master
     if: always()
     with:
       status: ${{ job.status }}
-      notify_when: 'failure' # default is 'success,failure,warnings'
+      notify_when: 'failure' # default: 'success,failure,warnings'
     env:
       SLACK_WEBHOOK_URL: ${{ secrets.SLACK_WEBHOOK_URL }} # required
+      COMMIT_MESSAGE: ${{ github.event.head_commit.message }} # get head commit message
+      GITHUB_TOKEN: ${{ secrets.PERSONAL_TOKEN }} # override authentication
 ```
 
-<sub>Made in Python &bull; By [Ravgeet Dhillon](https://github.com/ravgeetdhillon) @ [RavSam Web Solutions](https://www.ravsam.in).</sub>
+
+
+<sub>Originally Made in Python &bull; By [Ravgeet Dhillon](https://github.com/ravgeetdhillon) @ [RavSam Web Solutions](https://www.ravsam.in).</sub>
+<sub>Message customization By &bull; By [Yohanes](https://github.com/yohanesgultom).</sub>
--- main.py
+++ main.py
@@ -1,98 +1,120 @@
-import requests
 import os
-import json
 import sys
+import json
+import requests
+
+MESSAGE_USERNAME = "Github Action"
+MESSAGE_EMOJI = ":rocket:"
+MESSAGE_FOOTER = (
+    "By <https://github.com/yohanesgultom/notify-slack-action|notify-slack-action>"
+)
 
 
-def actionColor(status):
+def action_color(status):
     """
     Get a action color based on the workflow status.
     """
 
-    if status == 'success':
-        return 'good'
-    elif status == 'failure':
-        return 'danger'
+    if status == "success":
+        return "good"
+    elif status == "failure":
+        return "danger"
 
-    return 'warning'
+    return "warning"
 
 
-def actionStatus(status):
+def action_status(status):
     """
     Get a transformed status based on the workflow status.
     """
 
-    if status == 'success':
-        return 'passed'
-    elif status == 'failure':
-        return 'failed'
+    if status == "success":
+        return "SUCCESS"
+    elif status == "failure":
+        return "FAILED"
 
-    return 'passed with warnings'
+    return "WARNING"
 
 
-def actionEmoji(status):
+def action_emoji(status):
     """
     Get an emoji based on the workflow status.
     """
 
-    if status == 'success':
-        return ':sunglasses:'
-    elif status == 'failure':
-        return ':worried:'
+    if status == "success":
+        return ":heavy_check_mark:"
+    elif status == "failure":
+        return ":x:"
 
-    return ':zipper_mouth_face:'
+    return ":zipper_mouth_face:"
 
 
 def notify_slack(job_status, notify_when):
-    url = os.getenv('SLACK_WEBHOOK_URL')
-    workflow = os.getenv('GITHUB_WORKFLOW')
-    repo = os.getenv('GITHUB_REPOSITORY')
-    branch = os.getenv('GITHUB_REF')
-    commit = os.getenv('GITHUB_SHA')
+    """
+    Send message by calling Slack webhook URL
+    """
+    url = os.getenv("SLACK_WEBHOOK_URL")
+    workflow = os.getenv("GITHUB_WORKFLOW")
+    repo = os.getenv("GITHUB_REPOSITORY")
+    branch = os.getenv("GITHUB_REF")
+    commit = os.getenv("GITHUB_SHA")
+    author = os.getenv("GITHUB_ACTOR")
+    commit_message = os.getenv("COMMIT_MESSAGE")
 
-    commit_url = f'https://github.com/{repo}/commit/{commit}'
-    repo_url = f'https://github.com/{repo}/tree/{branch}'
+    commit_url = f"https://github.com/{repo}/commit/{commit}"
+    repo_url = f"https://github.com/{repo}/tree/{branch}"
 
-    color = actionColor(job_status)
-    status_message = actionStatus(job_status)
-    emoji = actionEmoji(job_status)
+    color = action_color(job_status)
+    status_message = action_status(job_status)
+    emoji = action_emoji(job_status)
 
-    message = f'{emoji} {workflow} {status_message} in <{repo_url}|{repo}@{branch}> on <{commit_url}|{commit[:7]}>.'
+    pretext = f"<{repo_url}|{repo} ({branch})>"
+    text = f"{emoji} {workflow}\n\n*"
+    text += f"• Status*: {status_message}\n"
+    text += f"• *Author*: {author}\n• *Commit*: <{commit_url}|{commit[:7]}>"
+    fallback = f"{workflow} {status_message} {repo_url} by {author}"
+    if commit_message:
+        text += f"\n• *Message*: {commit_message}"
 
     payload = {
-        'attachments': [
+        "username": MESSAGE_USERNAME,
+        "icon_emoji": MESSAGE_EMOJI,
+        "attachments": [
             {
-                'text': message,
-                'fallback': 'New Github Action Run',
-                'pretext': 'New Github Action Run',
-                'color': color,
-                'mrkdwn_in': ['text'],
-                'footer': 'Developed by <https://www.ravsam.in|RavSam>',
+                "text": text,
+                "fallback": fallback,
+                "pretext": pretext,
+                "color": color,
+                "mrkdwn_in": ["text"],
+                "footer": MESSAGE_FOOTER,
             }
-        ]
+        ],
     }
 
     payload = json.dumps(payload)
 
-    headers = {'Content-Type': 'application/json'}
+    headers = {"Content-Type": "application/json"}
 
     if notify_when is None:
-        notify_when = 'success,failure,warnings'
+        notify_when = "success,failure,warnings"
 
-    if job_status in notify_when and not testing:
+    if job_status in notify_when and not TESTING:
         requests.post(url, data=payload, headers=headers)
 
 
 def main():
-    job_status = os.getenv('INPUT_STATUS')
-    notify_when = os.getenv('INPUT_NOTIFY_WHEN')
+    """
+    Main method
+    """
+    job_status = os.getenv("INPUT_STATUS")
+    notify_when = os.getenv("INPUT_NOTIFY_WHEN")
     notify_slack(job_status, notify_when)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        testing = True if sys.argv[1] == '--test' else False
-    except IndexError as e:
-        testing = False
+        TESTING = sys.argv[1] == "--test"
+    except IndexError:
+        TESTING = False
 
     main()
